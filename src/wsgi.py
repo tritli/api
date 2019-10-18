25cf75706049b01e6312e2ac87737e1df16b9820
Roman <roman@lewandro.de>
2019-10-18 23:26:01 +0200

    adds configuration of host and port for flask through config file

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,8 +1,9 @@
 from api.trit_li import app, api, lfs, sfl, val, exp
+from config import HOST, PORT
 
 if __name__ == '__main__':
     api.add_namespace(lfs)
     api.add_namespace(sfl)
     api.add_namespace(val)
     api.add_namespace(exp)
-    app.run(debug=False, host='0.0.0.0', port='80')
+    app.run(debug=False, host=HOST, port=PORT)
