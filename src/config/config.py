5f0a92921c26f5ce5fef9e87a0eb98c805024ca2
Roman <roman@lewandro.de>
2019-10-18 23:20:52 +0200

    updates config files and comments

--- src/config/config.py
+++ src/config/config.py
@@ -1,10 +1,31 @@
+import string
 
-NODES = ["https://nodes.thetangle.org:443", "http://node05.iotatoken.nl:16265", "https://lewandroch.ddns.net"]
+# some sample nodes
+NODES = ["https://nodes.thetangle.org:443", "http://node05.iotatoken.nl:16265"]
 
-DOMAIN = "{}{}".format("http://", "trit.li/")
-PASSPHRASE = "TRITLI"
+# this is the domain used for creating the short URL
+DOMAIN = "{}{}".format("http://", "localhost/")
+
+# you can change the default salt used for validation
+TRITLI_SALT = "TRITLI"
+
+# default tag to find the url transactions on the tangle (allowed: 27 characters, A-Z and 9 only)
 TAG = "TRITLI"
+
+# address prefix, in case of changing the address hash to be backwards compatible
 ADDRESS_VERSION = "A"  # T = test, A = alpha
 
-API_USER = "admin@trit.li"
-API_PASS = "tritli"
+# parameters for creating the short URL
+SHORT_URL_LENGTH = 7
+SHORT_URL_CHARACTER_SET = string.ascii_letters + string.digits + "-"
+
+# http authentication for the API
+# requires usage of the decorator @auth.login_required in trit_li.py
+API_USER = "admin"
+API_PASS = "admin"
+
+# only affects the flask host and port inside the docker container
+# it does not affect the docker image itself
+# if you use docker, keep 0.0.0.0
+HOST = '0.0.0.0'
+PORT = 80
